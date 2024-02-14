bc0c78becbca6f310ae67ede761a6284c6f65fda
mihaylinsergey <semi999@mail.ru>
2024-02-14 08:55:02 +0300

    HomeWork module 2

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,6 @@
 pluginManagement {
     plugins {
-        val kotlinVersion : String by settings
+        val kotlinVersion: String by settings
         kotlin("jvm") version kotlinVersion
     }
 }
@@ -8,12 +8,7 @@ pluginManagement {
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
 }
+rootProject.name = "ok-marketplace-202312"
 
-rootProject.name = "otuskotlin-marketplace-202312"
-
-include("m1l1-first")
-include("m1l3-func")
-include("m1l4-oop")
-include("m1l5-dsl")
-include("m2l1-coroutines")
-include("m2l2-flows")
+includeBuild("lessons")
+includeBuild("ok-marketplace-be")
