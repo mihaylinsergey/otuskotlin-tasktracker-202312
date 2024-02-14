97f69ce6e67d8862a014fd5bfb21f49c4366a85b
mihaylinsergey <semi999@mail.ru>
2024-02-14 08:55:34 +0300

    HomeWork module 2

---
+++ lessons/settings.gradle.kts
@@ -0,0 +1,19 @@
+pluginManagement {
+    plugins {
+        val kotlinVersion : String by settings
+        kotlin("jvm") version kotlinVersion
+    }
+}
+
+plugins {
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
+}
+
+rootProject.name = "lessons"
+
+include("m1l1-first")
+include("m1l3-func")
+include("m1l4-oop")
+include("m1l5-dsl")
+include("m2l1-coroutines")
+include("m2l2-flows")
